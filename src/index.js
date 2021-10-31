f087cdbef6129a8f7b7c01c248f624dcd2794441
Anton <82974569+AntooxaD@users.noreply.github.com>
2021-10-31 14:37:55 +0200

    update

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './sass/main.scss';
+const debounce = require('lodash.debounce');
+import fetchCountries from './js/fetchCountries';
+import countryCard from './templates/country.hbs';
+import countriesCard from './templates/countries.hbs';
